a20b499f04163dbd01ba6d4ab156ab2aa8380d97
Vlir25 <vlir.project2023@gmail.com>
2025-06-25 13:43:33 -0500

    firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -2,25 +2,26 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-import { getRemoteConfig } from "firebase/remote-config";
+import { getRemoteConfig, fetchAndActivate, getValue } from "firebase/remote-config";
 
-// Configuración usando variables de entorno
+// Configuración de Firebase (copia esto desde Firebase Console)
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBRyI46nA8PnIy2hn-WevgTJG48qaDVw-I",
+  authDomain: "cavoundalfa-bfaa0.firebaseapp.com",
+  projectId: "cavoundalfa-bfaa0",
+  storageBucket: "cavoundalfa-bfaa0.appspot.com",
+  messagingSenderId: "979607031398",
+  appId: "1:979607031398:web:1dc4cf235844b2492c67a7",
+  measurementId: "G-MD1VV6TK28"
 };
 
+// Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 const remoteConfig = getRemoteConfig(app);
 
 // Opcional: Configurar Remote Config
-remoteConfig.settings.minimumFetchIntervalMillis = 0;
+remoteConfig.settings.minimumFetchIntervalMillis = 0; // Refrescar cada 1 hora 3600000
 
 export { db, collection, addDoc, remoteConfig };
